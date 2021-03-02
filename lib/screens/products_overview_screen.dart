9450496a9f6d958fe7b5ccf572f4ad077bb4fb0c
VendelinFilip <vendelinfilip73@gmail.com>
2021-03-02 14:55:29 +0100

    It's not like anybody's gonna read this anyway. Let's say I just did a bunch of shit.

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+
+class ProductsOverViewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ForMe'),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
